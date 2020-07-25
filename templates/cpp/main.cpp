34c0a47ff6e09f1fdd5ae3637b906d59fcda488f
AndreiIonica <andreiionica42@gmail.com>
2020-07-25 19:34:22 +0300

    fixed c++ template project

--- templates/cpp/main.cpp
+++ templates/cpp/main.cpp
@@ -5,14 +5,6 @@
 using namespace std;
 
 int main() {
-    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    int n;
-    cin >> n;
-    for (const string &word : msg) {
-        cout << word << " ";
-    }
-    cout << endl;
-    for (int i = 0; i < 5; i++)
-        cout << i << " ";
+    cout << "Hello World!" << endl;
     cout << endl;
 }
